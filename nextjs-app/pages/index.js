072caeeebc9c6f1e4282f503d039e66a374dd3c8
Iverson860216 <49859525+Iverson860216@users.noreply.github.com>
2023-07-13 23:01:36 +0800

    Update index.js

--- nextjs-app/pages/index.js
+++ nextjs-app/pages/index.js
@@ -3,16 +3,6 @@ import Layout, {siteTitle} from '../components/layout';
 // import utilStyles from '../styles/utils.module.css';
 // import {getSortedPostsData} from '../lib/posts';
 import DataBar from './posts/input';
-// import Post from './posts/[userInfo]';
-
-// export async function getStaticProps(){
-//   const allPostsData = getSortedPostsData();
-//   return {
-//     props:{
-//       allPostsData,
-//     }
-//   }
-// }
 
 export default function Home({allPostsData}) {
   return (
@@ -20,28 +10,7 @@ export default function Home({allPostsData}) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      {/* <section className={utilStyles.headingMd}> */}
-        {/* <p>[Hi,I'm Iverson]</p> */}
-        {/* <p>
-          <a href='https://nextjs.org/learn'>Next.js</a>
-        </p> */}
-      {/* </section> */}
-      {/* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section> */}
       <DataBar/>
-      {/* <Post/> */}
     </Layout>
   )
 }
